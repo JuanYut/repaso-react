c2ccbed43da1df6402a7b6086c3176dc026ef312
JuanYut <juanyut97@hotmail.com>
2019-09-28 14:32:53 -0500

    jest y enzyme

---
+++ src/UI/Dummy/Dummy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dummy = () => {
+  return (
+    <div>
+      <h3>Hi, I'm a Dummy :B</h3>;<span>Hello Batman ^u^</span>
+    </div>
+  );
+};
+
+export default Dummy;
